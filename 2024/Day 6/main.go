33648527f85c6812e2cbb6ff37c757aceaaa9dc4
InfosecWatchman <nathanlee.farmer@gmail.com>
2024-12-10 10:03:13 -0500

    2024 - Day 6 Part 2 incomplete

--- 2024/Day 6/main.go
+++ 2024/Day 6/main.go
@@ -26,93 +26,25 @@ func main() {
 			}
 		}
 	}
-
-	//fmt.Println(grid)
-	//fmt.Println(location)
-	/*
-		result := func() int {
-			var result int = 1
-			var count = 0
-			for {
-				switch strings.Split(lines[location[0]], "")[location[1]] {
-				case "^":
-					if location[0] != 0 {
-						if lines[location[0]-1] == "." {
-							result++
-							location[0] = location[0] - 1
-							line := strings.Split(lines[location[0]], "")[location[1]]
-							line = "."
-							lines = slices.Replace(lines, location[0], location[0], line)
-							previousline := strings.Split(lines[location[0]-1], "")[location[1]]
-							previousline = "^"
-							lines = slices.Replace(lines, location[0]-1, location[0]-1, previousline)
-						} else {
-							location[1] = location[1] + 1
-							strings.Split(lines[location[0]], "")[location[1]] = "."
-							strings.Split(lines[location[0]], "")[location[1]+1] = ">"
-						}
-					} else {
-						return result
-					}
-				case ">":
-					if location[1] != len(strings.Split(lines[location[0]], ""))-1 {
-						if lines[location[0]-1] == "." {
-							result++
-							location[0] = location[0] - 1
-							strings.Split(lines[location[0]], "")[location[1]] = "."
-							strings.Split(lines[location[0]], "")[location[1]+1] = ">"
-						} else {
-							location[1] = location[1] + 1
-							strings.Split(lines[location[0]], "")[location[1]] = "."
-							strings.Split(lines[location[0]+1], "")[location[1]] = "v"
-						}
-					} else {
-						return result
-					}
-				case "<":
-					if location[1] != 0 {
-						if lines[location[0]-1] == "." {
-							result++
-							location[0] = location[0] - 1
-							strings.Split(lines[location[0]], "")[location[1]] = "."
-							strings.Split(lines[location[0]], "")[location[1]-1] = "<"
-						} else {
-							location[1] = location[1] + 1
-							strings.Split(lines[location[0]], "")[location[1]] = "."
-							strings.Split(lines[location[0]+1], "")[location[1]] = "^"
-						}
-					} else {
-						return result
-					}
-				case "v":
-					if location[0] != len(lines)-1 {
-						if lines[location[0]-1] == "." {
-							result++
-							location[0] = location[0] - 1
-							strings.Split(lines[location[0]], "")[location[1]] = "."
-							strings.Split(lines[location[0]+1], "")[location[1]] = "v"
-						} else {
-							location[1] = location[1] + 1
-							strings.Split(lines[location[0]], "")[location[1]] = "."
-							strings.Split(lines[location[0]], "")[location[1]-1] = "<"
-						}
-					} else {
-						return result
-					}
-				}
-				fmt.Println(location)
-				if count >= 5 {
-					return result
-				} else {
-					count++
-				}
-			}
-		}()
-	*/
 	for _, line := range grid {
 		fmt.Println(line)
 	}
 
+	transformInput(grid, location, lines)
+	var newset string
+	for _, line := range grid {
+		fmt.Println(line)
+		newset = fmt.Sprintf("%s\n%s", newset, strings.Join(line, ""))
+	}
+	regex := regexp.MustCompile(`X`)
+	result := len(regex.FindAllString(newset, -1)) + 1
+	//fmt.Println(grid)
+
+	fmt.Printf("Part 1: %d\n", result)
+
+}
+
+func transformInput(grid [][]string, location []int, lines []string) {
 	for {
 		if grid[location[0]][location[1]] == "^" {
 			if location[0] == 0 {
@@ -172,15 +104,4 @@ func main() {
 			}
 		}
 	}
-	var newset string
-	for _, line := range grid {
-		fmt.Println(line)
-		newset = fmt.Sprintf("%s\n%s", newset, strings.Join(line, ""))
-	}
-	regex := regexp.MustCompile(`X`)
-	result := len(regex.FindAllString(newset, -1)) + 1
-	//fmt.Println(grid)
-
-	fmt.Printf("Part 1: %d\n", result)
-
 }
